b950b39c8da47fb7f6b83c533aced7175d020873
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-02-17 09:35:22 -0600

    org_tag variable add

--- models/tmp/stg_zendesk__organization_tag_tmp.sql
+++ models/tmp/stg_zendesk__organization_tag_tmp.sql
@@ -1,2 +1,5 @@
+--To disable this model, set the using_organization_tags variable within your dbt_project.yml file to False.
+{{ config(enabled=var('using_organization_tags', True)) }}
+
 select * 
 from {{ var('organization_tag') }}
