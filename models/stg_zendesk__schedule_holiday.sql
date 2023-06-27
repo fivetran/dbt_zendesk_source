8222c3b1736f39bb9f3f58b9bc7a2ab9405f83cf
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2023-06-27 15:10:58 -0500

    review updates

--- models/stg_zendesk__schedule_holiday.sql
+++ models/stg_zendesk__schedule_holiday.sql
@@ -22,8 +22,8 @@ fields as (
 final as (
     
     select
-        _fivetran_deleted,
-        _fivetran_synced,
+        cast(_fivetran_deleted as {{ dbt.type_timestamp() }} ) as _fivetran_deleted,
+        cast(_fivetran_synced as {{ dbt.type_timestamp() }} ) as _fivetran_synced,
         cast(end_date as {{ dbt.type_timestamp() }} ) as holiday_end_date_at,
         cast(id as {{ dbt.type_string() }} ) as holiday_id,
         name as holiday_name,
