2f1ba98bb945975fdc2c5fc9d4582fe8964084d4
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-02-29 06:38:16 -0800

    Update models/stg_zendesk__user.sql
    Co-authored-by: Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>

--- models/stg_zendesk__user.sql
+++ models/stg_zendesk__user.sql
@@ -37,7 +37,10 @@ final as (
         email,
         name,
         organization_id,
-        case when phone in ('-', 'null', 'NA') then null else phone end as phone,
+        case when phone in ('-', 'null', 'NA') 
+            then null 
+            else phone 
+        end as phone,
         {% if var('internal_user_criteria', false) -%}
             case 
                 when role in ('admin', 'agent') then role
