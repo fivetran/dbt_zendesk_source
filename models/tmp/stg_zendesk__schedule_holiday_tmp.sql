f6740d12ef1335369d9824bc3f95e85ac1482aa9
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-09-30 10:22:49 -0500

    add using_holidays

--- models/tmp/stg_zendesk__schedule_holiday_tmp.sql
+++ models/tmp/stg_zendesk__schedule_holiday_tmp.sql
@@ -1,5 +1,5 @@
 --To disable this model, set the using_schedules variable within your dbt_project.yml file to False.
-{{ config(enabled=var('using_schedules', True)) }}
+{{ config(enabled=var('using_schedules', True) and var('using_holidays', True)) }}
 
 select {{ dbt_utils.star(source('zendesk', 'schedule_holiday')) }}
 from {{ source('zendesk', 'schedule_holiday') }} as schedule_holiday_table
