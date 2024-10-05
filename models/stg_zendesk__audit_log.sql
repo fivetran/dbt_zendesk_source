8e4d566ad052eeeb5dc4a2ad0f207bdb61e61ede
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-10-05 17:13:27 -0500

    Update vars

--- models/stg_zendesk__audit_log.sql
+++ models/stg_zendesk__audit_log.sql
@@ -1,4 +1,5 @@
 {{ config(enabled=fivetran_utils.enabled_vars(['using_schedules','using_schedule_histories'])) }}
+
 with base as (
 
     select * 
--- models/stg_zendesk__schedule_holiday.sql
+++ models/stg_zendesk__schedule_holiday.sql
@@ -1,5 +1,5 @@
---To disable this model, set the using_schedules variable within your dbt_project.yml file to False.
-{{ config(enabled=var('using_schedules', True) and var('using_holidays', True)) }}
+--To disable this model, set the using_schedules or using_holidays variable within your dbt_project.yml file to False.
+{{ config(enabled=fivetran_utils.enabled_vars(['using_schedules','using_holidays'])) }}
 
 with base as (
 
