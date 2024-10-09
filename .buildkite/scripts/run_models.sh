903b757ae4e2a2cd33a18cd93a324a7f18ac4bca
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-10-09 17:10:26 -0500

    update run models

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,6 +19,8 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
-dbt run --vars '{using_schedules: false, using_schedule_histories: false, using_holidays:false, using_domain_names: false, using_user_tags: false, using_ticket_form_history: false, using_organization_tags: false}' --target "$db" --full-refresh
+dbt run --vars '{using_schedules: false, using_domain_names: false, using_user_tags: false, using_ticket_form_history: false, using_organization_tags: false}' --target "$db" --full-refresh
+dbt test --target "$db"
+dbt run --vars '{using_schedule_histories: true, using_holidays: false}' --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
